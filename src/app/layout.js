f51a398b7a45954b82e98e965f17bebeb7c6572b
arburhan <burhan128454@outlook.com>
2024-02-05 07:44:39 +0600

    change structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,13 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
-        <Providers>
-          <main>
+        <main>
+          <Providers>
             {children}
-          </main>
-        </Providers>
+          </Providers>
+        </main>
       </body>
     </html>
 
