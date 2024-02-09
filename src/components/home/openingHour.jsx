10f8df5673c2d26e08700eafc14e82e84f400078
arburhan <burhan128454@outlook.com>
2024-02-09 22:52:00 +0600

    library schedule added

--- src/components/home/openingHour.jsx
+++ src/components/home/openingHour.jsx
@@ -1,14 +1,68 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import Title from '../shared/title';
-
-
+import moment from 'moment';
+import { Chip } from "@nextui-org/react";
+import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
 
 const OpeningHour = () => {
+    const [offDay, setOffDay] = useState(false);
+    const currentDates = moment().format('Do MMMM YYYY');
+    const currentTime = moment().format('LT');
+    const days = moment().format('dddd');
+    useEffect(() => {
+        if (days === "Friday" || days === "Saturday") {
+            setOffDay(true);
+        } else {
+            setOffDay(false);
+        }
+    }, [days]);
+
     return (
         <section>
-            <Title name={"Library Hour"} />
-            <div>
+            <Title name={"Library Schedule"} />
+            <div className='flex justify-center items-center gap-x-5 pb-3'>
+                <div>
+                    Today
+                </div>
+                <div className={`pl-2 border-l-4 ${offDay ? 'border-red-600' : 'border-green-600'}`}>
+                    <div className='py-3'>
+                        <p className={`${offDay ? 'text-red-600' : 'text-green-600'}`}>{days}</p>
+                        <p>{currentDates}</p>
+                        <p>{currentTime}</p>
+                    </div>
+                </div>
 
+            </div>
+            <div className='text-center'>
+                <div>
+                    <h3> <span className='text-cyan-500'>Morning Shift:</span> 09:00 AM - 01:30 PM </h3>
+                    <div className='py-2'>
+                        Status:
+                        <span className='pl-3' >
+                            <Chip
+                                startContent={offDay ? <AiFillCloseCircle /> : <AiFillCheckCircle />}
+                                variant="faded"
+                                color={`${offDay ? "danger" : "success"}`}>
+                                {offDay ? 'Close' : "Open"}
+                            </Chip>
+                        </span>
+                    </div>
+                </div>
+                <div className='pt-2'>
+                    <h3> <span className='text-cyan-500'>Evening Shift:</span> 02:00 PM - 05:00 PM </h3>
+                    <div className='py-2'>
+                        Status:
+                        <span className='pl-3' >
+                            <Chip
+                                startContent={offDay ? <AiFillCloseCircle /> : <AiFillCheckCircle />}
+                                variant="faded"
+                                color={`${offDay ? "danger" : "success"}`}>
+                                {offDay ? 'Close' : "Open"}
+                            </Chip>
+                        </span>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- src/components/shared/title.jsx
+++ src/components/shared/title.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import moment from 'moment';
+
 
 const Title = ({ name }) => {
-    const currentDates = moment().format('Do MMMM YYYY');
-    const currentTime = moment().format('LT');
-    const days = moment().format('dddd');
-
     return (
-
         <div className="text-center my-10" >
             <h1 className="text-3xl" >{name}</h1>
-            <div className='flex justify-center items-center gap-x-5'>
-                <div>
-                    Today
-                </div>
-                <div>
-                    <div className='py-3'>
-                        <p className={`${days ? 'text-red-600' : 'text-green-600'}`}>{days}</p>
-                        <p>{currentDates}</p>
-                        <p>{currentTime}</p>
-                    </div>
-                </div>
-
-            </div>
-
         </div>
-
     );
 };
 
