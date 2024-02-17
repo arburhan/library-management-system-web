c8c83272839a3396ee872e8629d53e7eed2b014e
arburhan <burhan128454@outlook.com>
2024-02-17 21:34:02 +0600

    add space

--- src/components/home/recentbooks/recentbooks.jsx
+++ src/components/home/recentbooks/recentbooks.jsx
@@ -6,14 +6,14 @@ import { Button } from '@nextui-org/react';
 import Link from 'next/link';
 const Recentbooks = ({ page = false }) => {
     return (
-        <section className='mb-3'>
+        <section className='my-6'>
             <Title name={"Recently added books"} />
             <div className="grid grid-cols-3 gap-3 md:grid md:grid-cols-4 mx-[50px] md:mx-[100px] justify-evenly items-center md:gap-6 my-3">
                 {
                     recentbooks.map(book => <RecentbooksCard key={book._id} book={book} />)
                 }
             </div>
-            <div className={`text-center ${page == true ? "hidden" : "block"}`}>
+            <div className={`text-center ${page == true ? "hidden" : "block"} py-2`}>
                 <Link href="/recentbooks">
                     <Button radius="xl" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg text-md p-4">
                         All recent books
