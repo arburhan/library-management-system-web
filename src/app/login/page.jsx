b54f4cc4502340273a1ab02e09bc91d28fcec3e5
arburhan <burhan128454@outlook.com>
2024-03-04 18:12:36 +0600

    add react hook form for validation

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,80 +5,78 @@ import { Button, Input } from "@nextui-org/react";
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import Title from '@/components/shared/title';
 import Link from 'next/link';
+import { useForm } from "react-hook-form";
 
 const page = () => {
-    const [value, setValue] = useState(" ");
-    const [isVisible, setIsVisible] = useState(false);
-    const [touched, setTouched] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-    const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+    const onSubmit = data => {
+        console.log(data);
+    };
 
-    const isInvalid = useMemo(() => {
-        if (value === "") return false;
-
-        return validateEmail(value) ? false : true;
-    }, [value]);
-
-    const toggleVisibility = () => setIsVisible(!isVisible);
-
-    const handleBlur = () => setTouched(true);
 
     return (
-        <section className='h-screen '>
+        <section className='h-screen'>
             <Title name={"Login"} />
-            <div className='flex flex-col w-full  md:w-[400px]'>
-                <div className='py-3'>
-                    <div className="label ml-2 pb-1">
-                        <span className="text-[14px]">Email</span>
-                    </div>
+            <div className='flex flex-col items-center'>
+                <form className=' w-full  md:w-[400px] px-4 md:px-2' onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <Input
-                            type="email"
-                            label="Email"
-                            variant="bordered"
-                            isInvalid={isInvalid && touched}
-                            errorMessage={isInvalid && touched && "enter a valid email"}
-                            onValueChange={setValue}
-                            onBlur={handleBlur}
-                            className="w-full max-w-md"
-                        />
+                        <div className="label ml-2 pb-1">
+                            <span className="text-[14px]">Email</span>
+                        </div>
+                        <div>
+                            <Input
+                                type="email"
+                                label="Email"
+                                variant="bordered"
+                                {...register('email', {
+                                    required: 'Email is required',
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: 'Invalid email address'
+                                    }
+                                })}
+                                className="w-full max-w-md"
+                            />
+                            {errors.email && <small className='text-red-500 ml-1' >{errors.email.message}</small >}
+                        </div>
                     </div>
-                </div>
-                <div className='pt-3'>
-                    <div className="label ml-2 pb-1">
-                        <span className="text-[14px]">Password</span>
+                    <div className='py-2'>
+                        <div className="label ml-2 pb-1">
+                            <span className="text-[14px]">Password</span>
+                        </div>
+                        <div>
+                            <Input
+                                type="password"
+                                label="Password"
+                                variant="bordered"
+                                {...register('password', {
+                                    required: 'Password is required',
+                                    minLength: {
+                                        value: 8,
+                                        message: 'Password must have at least 8 characters'
+                                    }
+                                })}
+                                className="w-full max-w-md"
+                            />
+                            {errors.password && <small className='text-red-500 ml-1' >{errors.password.message}</small >}
+                        </div>
                     </div>
-                    <div>
-                        <Input
-                            label="Password"
-                            variant="bordered"
-                            endContent={
-                                <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
-                                    {isVisible ? (
-                                        <AiFillEye className="text-2xl text-default-400 pointer-events-none" />
-
-                                    ) : (
-                                        <AiFillEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
-
-                                    )}
-                                </button>
-                            }
-                            type={isVisible ? "text" : "password"}
-                            className="w-full max-w-md"
-                        />
+                    <div className="label ml-2">
+                        <span className="text-[14px] cursor-pointer text-warning-500">Forget Password?</span>
                     </div>
-                </div>
-                <div className="label ml-2 p-1">
-                    <span className="text-[14px] cursor-pointer text-warning-500">Forget Password?</span>
-                </div>
-                <Button radius="md" className='mt-5'>
-                    Login
-                </Button>
+                    <Button type="submit" radius="md" className='mt-4 w-full'>
+                        Login
+                    </Button>
+                </form>
                 <div className="label ml-2 p-1">
                     <span className="text-[14px]">New Here? <Link href='/signup'><span className='cursor-pointer text-orange-200 hover:text-warning-500'>Sign up Now</span></Link> </span>
                 </div>
                 <div>
-
                 </div>
             </div>
         </section>
